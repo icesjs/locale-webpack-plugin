b9058e13385cd86494d817e20bbc7fa6ac8cd0f5
Stone <star2018@outlook.com>
2021-01-15 12:43:22 +0800

    fix: add polyfill to support lower version of node.js runtime

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 import LocalePlugin from './Plugin'
 export = LocalePlugin
+
+import 'ts-polyfill/lib/es2017-object'
+import 'ts-polyfill/lib/es2017-string'
+import 'ts-polyfill/lib/es2018-async-iterable'
+import 'ts-polyfill/lib/es2018-promise'
+import 'ts-polyfill/lib/es2019-array'
+import 'ts-polyfill/lib/es2019-object'
+import 'ts-polyfill/lib/es2019-string'
+import 'ts-polyfill/lib/es2019-symbol'
+import 'ts-polyfill/lib/es2020-promise'
+import 'ts-polyfill/lib/es2020-string'
+import 'ts-polyfill/lib/es2020-symbol-wellknown'
